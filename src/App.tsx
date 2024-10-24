7546c4b4d863aa9ed8f0dc4e445439a5961e0906
David Roussov <david.roussov@protonmail.com>
2024-10-24 11:38:54 +1100

    add loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,53 @@ import { css } from '@emotion/css';
 
 function App() {
   const [ files, setFiles ] = useState<File[]>([]);
+  const [ loading, setLoading ] = useState(false);
 
   const handleSubmitFiles = async (): Promise<void> => {
+    setLoading(true);
+
     throw new Error('Unimplemented');
   };
 
   return (
     <div className={css`padding: 100px 200px;`}>
       <h1>Welcome</h1>
-      <form
-        className={css`
-          display: flex;
-          flex-direction: column;
-        `}
-        onSubmit={(event: FormEvent<HTMLFormElement>) => {
-          event.preventDefault();
-          handleSubmitFiles();
-        }}
-      >
-        <FileUpload
-          files={files}
-          onChange={(newFiles: Files[]) => {
-            setFiles(newFiles);
+      { loading ?
+        <div>
+          Loading...
+        </div> :
+        <form
+          className={css`
+            display: flex;
+            flex-direction: column;
+          `}
+          onSubmit={(event: FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            handleSubmitFiles();
           }}
-        />
-        <div className={css`
-          display: flex;
-          justify-content: right;
-        `}>
-          <button
-            type="submit"
-            className={css`
-              margin-top: 10px;
-              cursor: pointer;
-            `}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+        >
+          <FileUpload
+            files={files}
+            onChange={(newFiles: Files[]) => {
+              setFiles(newFiles);
+            }}
+          />
+          <div className={css`
+            display: flex;
+            justify-content: right;
+          `}>
+            <button
+              type="submit"
+              className={css`
+                margin-top: 10px;
+                cursor: pointer;
+              `}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      }
     </div>
   );
 }
