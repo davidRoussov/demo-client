0f25445dcc6d49f5fd750df6ca2718c80880b7ef
David Roussov <david.roussov@protonmail.com>
2024-10-24 11:51:05 +1100

    upload file to server

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,31 @@ function App() {
   const [ loading, setLoading ] = useState(false);
 
   const handleSubmitFiles = async (): Promise<void> => {
-    setLoading(true);
+    if (files.length > 0) {
+      setLoading(true);
 
-    throw new Error('Unimplemented');
+      const firstFile = files[0];
+
+      let formData = new FormData();
+      formData.append('file', firstFile);
+
+      try {
+        const response = await fetch(`${import.meta.env.VITE_SERVER_BASE_URL}/upload`, {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (!response.ok) {
+          throw new Error('Upload failed');
+        }
+
+        console.info('Upload successful');
+      } catch(error) {
+        console.error('Upload failed', error);
+      } finally {
+        setLoading(false);
+      }
+    }
   };
 
   return (
